6aca22739acb61e4cd8f0d0e3dbb165b8dc5d883
immortalstr <immortalstr1990@mail.ru>
2023-01-31 20:57:18 +0300

    Add zada4i

--- Classwork/For_adn_While.py
+++ Classwork/For_adn_While.py
@@ -1,7 +1,11 @@
-# for i in range(10):
+# for i in range(10, 100,2):
 #     print(i)
 
 idx = 0
 while idx <= 9:
+    idx += 1
+    if idx ==7:
+        break
     print(idx)
+    # break - остановит после первой итерации 
     idx+= 2
