7a29e081a1357f2940fc43b3ba5810c552db4fde
immortalstr <immortalstr1990@mail.ru>
2023-02-02 15:57:51 +0300

    Modify zada4a-12

--- Task02/Zada4a-12.py
+++ Task02/Zada4a-12.py
@@ -7,9 +7,29 @@
 # 4 4 -> 2 2
 # 5 6 -> 2 3
 from math import *
+# first_number, second_number = int(input("Сумма чисел: ")), int(input("Произведение чисел: "))
+# for i in range(1, first_number+1):
+#     for j in range(1, first_number+1):
+#         if i + j == first_number and i * j == second_number:
+#             print(f"Задуманные Петей Числа {first_number} {second_number} -> {i} {j}")
 
-first_number, second_number = int(input("Сумма чисел: ")), int(input("Произведение чисел: "))
-for i in range(1, first_number+1):
-    for j in range(1, first_number+1):
-        if i + j == first_number and i * j == second_number:
-            print(f"Задуманные Петей Числа {first_number} {second_number} -> {i} {j}")
+def find_numbers(S, P):
+    D = S**2 - 4*P
+    if D < 0:
+        return None
+
+    X1 = (S + sqrt(D)) / 2
+    X2 = (S - sqrt(D)) / 2
+
+    if X1 > 0 and X1.is_integer() and X2 > 0 and X2.is_integer():
+        return (int(X1), int(X2))
+    elif X1 > 0 and X1.is_integer():
+        return (int(X1),)
+    elif X2 > 0 and X2.is_integer():
+        return (int(X2),)
+    else:
+        return None
+
+S=int(input("Введите первое число "))
+P=int(input("Введите второе число "))
+print(find_numbers(S, P))
