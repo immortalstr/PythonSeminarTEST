f210ec2a0de660d5a276ab7359ce695db676dd94
immortalstr <immortalstr1990@mail.ru>
2023-02-21 21:36:26 +0300

    update2

--- Classwork/Classwork-8/2.py
+++ Classwork/Classwork-8/2.py
@@ -1,6 +1,12 @@
 def print_operation_table(operation, num_rows=6, num_columns=6):
+    print("    ",end="")
     for i in range(1, num_rows + 1):
+        print("{:4}".format(i),end=" ")
+    print()
+    print("     ",'-'* 4 * (num_columns + 1))
+    for i in range(1, num_rows + 1):
+        print(i,"|",end=" ")
         for j in range(1, num_columns + 1):
             print(str(operation(i, j)).rjust(4), end =' ')
         print()
-print(print_operation_table(lambda x, y: x**y, 4, 4))
+print_operation_table(lambda x, y: x**y, 6, 6)
